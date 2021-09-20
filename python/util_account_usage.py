a728b3245ecc92ae9c70c973bd6e657e81e4b5e5
Yuya Tinnefeld <yuyathomas-tinnefelde@rewe-group.com>
2021-09-20 17:10:17 +0200

    updates python api

--- python/util_account_usage.py
+++ python/util_account_usage.py
@@ -2,6 +2,14 @@ import snowflake.connector.connection as conn
 import snowflake.connector.cursor as cursor
 import pandas as pd
 
+def get_account_usage_views(cs:cursor.SnowflakeCursor)-> list:
+    show_views_query = "SHOW views IN SNOWFLAKE.ACCOUNT_USAGE"
+    all_views = cs.execute(show_views_query)
+    all_view_names = [view[1] for view in all_views]
+
+    return all_view_names
+ 
+
 def account_usage(cs:cursor.SnowflakeCursor, db_object:str) -> pd.DataFrame:
     show_col_query = f"SHOW COLUMNS IN TABLE SNOWFLAKE.ACCOUNT_USAGE.{db_object}"
     columns_schema = cs.execute(show_col_query)
